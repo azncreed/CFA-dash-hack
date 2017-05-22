f85eeca682430bd6f953dd779b060581f8f9fa01
Sylvia Shen <azncreedii@gmail.com>
2017-05-23 08:01:49 +1000

    add bookingsindexcomp into dashboard

---
+++ sams-dashboard/src/BookingsIndexComp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const BookingsIndexComp = (props) => {
+  return (
+  	<p>{props.bookings.length}</p>
+  	)
+};
+
+
+
+
+
+export default BookingsIndexComp;
