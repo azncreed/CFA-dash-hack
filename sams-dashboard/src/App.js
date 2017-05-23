37e3419f5d428c95bbefbe8fc5cf74deaa6de07d
Sylvia Shen <azncreedii@gmail.com>
2017-05-23 10:48:49 +1000

    add cards for dashboard

--- sams-dashboard/src/App.js
+++ sams-dashboard/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import ClientCount from './ClientCount';
 import BookingsIndexComp from './BookingsIndexComp';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
@@ -14,16 +15,33 @@ class App extends Component {
   };
 
 
-
-
   render() {
     return (
       <div>
-        <h1>Personal Dashboard</h1>
+        <h1 className="title">Personal Dashboard</h1>
 
-       <ClientCount clients={this.state.clients} />
+        <Row>
+        <Col sm="6">
+          <Card block className="clients">
+            <CardTitle>Number of Clients</CardTitle>
+             <CardText>
+              <ClientCount clients={this.state.clients} />
+             </CardText>
+        </Card>
+       </Col>
 
-       <BookingsIndexComp bookings={this.state.bookings} />
+        <Col sm="6">
+         <Card block className="booking-history">
+          <CardTitle>Booking History</CardTitle>
+           <CardText>
+           <BookingsIndexComp bookings={this.state.bookings} />
+           </CardText>
+         </Card>
+        </Col>
+        </Row>
+
+        <Row>
+        </Row>
        
       </div>
     );
